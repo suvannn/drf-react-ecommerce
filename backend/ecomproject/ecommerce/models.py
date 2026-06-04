41df244c069a066ecc5b9f179394d26a6d8b8460
Suvan Magar <suvanmagar7@gmail.com>
2026-06-04 16:45:51 +0545

    Added Redux

--- backend/ecomproject/ecommerce/models.py
+++ backend/ecomproject/ecommerce/models.py
@@ -10,7 +10,7 @@ class Products(models.Model):
     product_info = models.TextField(null=True, blank=True)
     rating = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
     numReviews = models.IntegerField(null=True, blank=True, default=0)
-    price = models.DecimalField(max_digits=7, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     count_in_stock = models.IntegerField(null=True, blank=True, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     _id = models.AutoField(primary_key=True, editable=False)
